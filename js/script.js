e3266f52689771f1fa1abb5375048f14f2f1f530
oluwafemi <oluwafemiogunleye21@gmail.com>
2022-06-12 12:12:33 +0100

    add fadeIn and fadeOut

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
+// $(document).ready(function(){
+//     $(".clickable").click(function(){
+//         $(".finegirl").slideDown();
+//         $(".finegirl").slideUp();
+//     });
+// });
 $(document).ready(function(){
     $(".clickable").click(function(){
-        $(".finegirl").slideDown();
-        $(".finegirl").slideUp();
+        $(".finegirl").fadeIn();
+        $(".finegirl").fadeOut();
     });
 });
